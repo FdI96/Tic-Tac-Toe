eb75830b6404a25ac15733863234d46602592437
FdI96 <federicolopezce@gmail.com>
2021-02-04 19:06:34 -0300

    Update main.rb, easy flow from the user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,45 +1,134 @@
+#!/usr/bin/env ruby
 puts 'WELCOME'
 puts 'This is my TIC TAC TOE game, hope you enjoy it'
-
-puts 'Please type 1 if you want to play alone, or 2 to play with other player!'
-response = (gets.chomp)
-
-if response.to_n == 1
-    # play vs the computer
-    puts 'Player one name:'
-    pone = gets.chomp
-    player_one = pone #Here I have to create a new Player class instance
-    puts "Hello #{player_one}, this is 'CPU' and it will be your opponent"
-    board = 'This is the board instance class' #Here I have to create a new Board class instance
-    #board.display
-    puts board
-    puts 'Displaying empty board' #I have to display the board through the display method from the board
-    puts "Press 'P' to start"
-    key = gets.chomp
-    if key == 'P'
-        #Start the game vs the computer
-        game = true
-        win = false
-        while game
-            
-            puts "Your turn, select one number to put a 'X'"
-            puts board
-            # here I set up the board and the array that will manage the game
-            number = gets.chomp
-            array = [1,2,3,4,5,6,7,8,9]
-            if array.include?(number)
-                puts "Change the position #{number} for an X"
-                board
-                puts 'check if there is one combination completed'
-                return puts 'X wins' if win
-                
-
-        end
-    else
-        puts 'Thanks for playing'
+updated_board = 'Board Updated.........'
+empty_board = 'Displaying empty board'
+puts "Press 'S' to start"
+response = gets.chomp
+board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# combination_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1 ,4 ,7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 6]]
+if response == 'S'
+  # play vs other player
+  puts 'Creating Game instance.......'
+  puts 'Player one name:'
+  pone = gets.chomp
+  player_one = pone # Here I have to create a new Player class instance
+  puts 'Creating Player 1 Instance'
+  puts 'Player two name:'
+  ptwo = gets.chomp
+  player_two = ptwo # Here I have to crate a New Player Instance
+  puts "Hello #{player_one} and #{player_two} ready to start the game?"
+  board = puts 'This is the board instance class' # Here I have to create a new Board class instance
+  # board.display
+  puts board
+  puts empty_board # I have to display the board through the display method from the board
+  puts "Press 'P' to start"
+  key = gets.chomp
+  if key == 'P'
+    # Start the game vs player 2
+    puts "#{player_one} please press H to select 'Heads' or T to select 'Tails'"
+    player_coin = gets.chomp
+    if player_coin == 'H'
+      player_coin = 'heads'
+    elsif player_coin == 'T'
+      player_coin = 'tails'
     end
-elsif response.to_n == 2
-    #play with other friend
+    coin = rand.round.zero? ? 'heads' : 'tails'
+    puts coin
+    puts player_coin
+    if player_coin == coin
+      (1..9).each do |elem|
+        if [1, 3, 5, 7, 9].include?(elem)
+          puts "Player #{player_one}'s turn!"
+          band = false
+          until band # In case that the slot is filled I need to restart the loop
+            puts 'Please select one empty space'
+            puts updated_board
+            number = gets.chomp.to_i
+            if all.include?(number) # I ask if the number selected is in the (1..9) range
+              if board_array[number - 1] == 'X' || board_array[number - 1] == 'O'
+                puts 'Slot occupied'
+              else # analize in the following lines if the next move is a win move or a draw move
+                board_array[number - 1] = 'X'
+                puts updated_board
+                print board_array, "\n"
+                band = true # Stops the loop to let the player's next turn
+              end
+            else
+              puts 'No such position'
+            end
+          end
+        else
+          puts "Player #{player_two}'s turn!"
+          band = false
+          until band # In case that the slot is filled I need to restart the loop
+            puts 'Please select one empty space'
+            puts updated_board
+            number = gets.chomp.to_i
+            if all.include?(number) # I ask if the number selected is in the (1..9) range
+              if board_array[number - 1] == 'O' || board_array[number - 1] == 'X'
+                puts 'Slot occupied'
+              else # analize in the following lines if the next move is a win move or a draw move
+                board_array[number - 1] = 'O'
+                puts updated_board
+                print board_array, "\n"
+                band = true # Stops the loop to let the player's next turn
+              end
+            else
+              puts 'No such position'
+            end
+          end
+        end
+      end
+    elsif player_coin != coin
+      (1..9).each do |elem|
+        if [1, 3, 5, 7, 9].include?(elem)
+          puts "Player #{player_two}'s turn!"
+          band = false
+          until band # In case that the slot is filled I need to restart the loop
+            puts 'Please select one empty space'
+            puts updated_board
+            number = gets.chomp.to_i
+            if all.include?(number) # I ask if the number selected is in the (1..9) range
+              if board_array[number - 1] == 'O' || board_array[number - 1] == 'X'
+                puts 'Slot occupied'
+              else # analize in the following lines if the next move is a win move or a draw move
+                board_array[number - 1] = 'O'
+                puts updated_board
+                print board_array, "\n"
+                band = true # Stops the loop to let the player's next turn
+              end
+            else
+              puts 'No such position'
+            end
+          end
+        else
+          puts "Player #{player_one}'s turn!"
+          band = false
+          until band # In case that the slot is filled I need to restart the loop
+            puts 'Please select one empty space'
+            puts updated_board
+            number = gets.chomp.to_i
+            if all.include?(number) # I ask if the number selected is in the (1..9) range
+              if board_array[number - 1] == 'X' || board_array[number - 1] == 'O'
+                puts 'Slot occupied'
+              else # analize in the following lines if the next move is a win move or a draw move
+                board_array[number - 1] = 'X'
+                puts updated_board
+                print board_array, "\n"
+                band = true # Stops the loop to let the player's next turn
+              end
+            else
+              puts 'No such position'
+            end
+          end
+        end
+      end
+    end
+  else
+    puts 'Execute the game again to play it'
+  end
 else
-    raise WrongOption
+  raise WrongOption
 end
