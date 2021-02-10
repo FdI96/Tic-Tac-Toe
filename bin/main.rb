4d22a066317dc72a70400b1cfcbd3212036b32f2
FdI96 <federicolopezce@gmail.com>
2021-02-10 18:01:26 -0300

    Fixing issues and putting standar config

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
-# Temporary methods ------------------------------------------------------------------------------
 
-# Shows board
 def updated_board(board_array)
   puts "#{board_array[0]} | #{board_array[1]} | #{board_array[2]}"
   puts '-- --- --'
@@ -10,7 +8,6 @@ def updated_board(board_array)
   puts "#{board_array[6]} | #{board_array[7]} | #{board_array[8]}"
 end
 
-# Shows empty board
 def empty_board
   puts '1 | 2 | 3'
   puts '-- --- --'
@@ -19,14 +16,12 @@ def empty_board
   puts '7 | 8 | 9'
 end
 
-# win or draw logic
-
-def win_or_draw(accu, player_two, player_one, turn)
+def win_or_draw(accu)
   random = rand(0..100)
   return true unless accu >= 4
 
   if random < 25
-    puts turn ? "#{player_one} WINs!" : "#{player_two} WINs!"
+    puts 'Player X WINS'
     false
   elsif random < 50
     puts 'Its a DRAW'
@@ -39,25 +34,28 @@ def win_or_draw(accu, player_two, player_one, turn)
   end
 end
 
-# Player one turn method
+def select_slot(band, turn, board_array)
+  puts 'Please select one empty space'
+  updated_board(board_array)
+  number = gets.chomp.to_i
+  if (1..9).include?(number)
+    if board_array[number - 1] == 'X' || board_array[number - 1] == 'O'
+      puts 'Slot occupied'
+    else
+      puts "TURN VALUE #{turn}"
+      board_array[number - 1] = turn ? 'X' : 'O'
+      updated_board(board_array)
+      band = true
+    end
+  else
+    puts 'No such position'
+  end
+  band
+end
+
 def player_turn(board_array, turn)
   band = false
-  until band
-    puts 'Please select one empty space'
-    updated_board(board_array)
-    number = gets.chomp.to_i
-    if (1..9).include?(number)
-      if board_array[number - 1] == 'X' || board_array[number - 1] == 'O'
-        puts 'Slot occupied'
-      else
-        board_array[number - 1] = turn ? 'X' : 'O'
-        updated_board(board_array)
-        band = true
-      end
-    else
-      puts 'No such position'
-    end
-  end
+  band = select_slot(band, turn, board_array) until band
 end
 
 puts 'WELCOME'
@@ -65,7 +63,6 @@ puts 'This is my TIC TAC TOE game, hope you enjoy it'
 puts "Press 'S' to start"
 response = gets.chomp
 board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 s_array = %w[S s]
 p_array = %w[P p]
 raise WrongOption unless s_array.include? response
@@ -94,39 +91,23 @@ if p_array.include? key
   end
   coin = rand.round.zero? ? 'heads' : 'tails'
   accu = 0
-  if player_coin == coin
-    game_on = true
+  turn = coin == 'head'
+  game_on = true
+  if player_coin == coin    
     while game_on && accu < 9
-      turn = true
-      game_on = true
-      puts "Player #{player_one}'s turn!"
-      player_turn(board_array, all, turn)
-      game_on = win_or_draw(accu, player_two, player_one, turn)
-      break if game_on == false
-
-      accu += 1
-      turn = false
-      puts "Player #{player_two}'s turn!"
+      puts 'Player X turn!'
       player_turn(board_array, turn)
-      game_on = win_or_draw(accu, player_two, player_one, turn)
+      game_on = win_or_draw(accu)
       accu += 1
+      turn = !turn
     end
   elsif player_coin != coin
-    game_on = true
     while game_on && accu < 9
-      turn = false
-      game_on = true
-      puts "Player #{player_two}'s turn!"
+      puts 'Player X turn!'
       player_turn(board_array, turn)
-      game_on = win_or_draw(accu, player_two, player_one, turn)
-      break if game_on == false
-
+      game_on = win_or_draw(accu)
       accu += 1
-      turn = true
-      puts "Player #{player_one}'s turn!"
-      player_turn(board_array, turn)
-      game_on = win_or_draw(accu, player_two, player_one, turn)
-      accu += 1
+      turn = !turn
     end
   end
 else
