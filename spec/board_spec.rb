66c025227ab67af7a2bddfff8500f4f4cd5f2ea6
FdI96 <federicolopezce@gmail.com>
2021-02-22 13:36:08 -0300

    Update board_spec.rb
    Adding 3 test for win_or_draw method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 
-CA = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-B = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-board = Board.new(B, CA)
+ca = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+testa = [%w[X X X], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+testb = [%w[X O X], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+board = Board.new(b, ca)
 
 describe Board do
   describe '#turn' do
@@ -19,17 +21,33 @@ describe Board do
       expect(board.turn(false, player_one, player_two)).to eql('FEDE2')
     end
   end
-  
+
   describe '#inside_array' do
     it "return the user input - 1, if it's included in the smaller array" do
       expect(board.inside_array([0, 1, 2], 3)).to include 2
     end
   end
-  
+
   describe '#modify_board' do
-    it "return true if the selected number is available" do
-      B = [1, 2, 'X', 4, 5, 'O', 7, 8, 9]
+    it 'return true if the selected number is available' do
+      B = [1, 2, 'X', 4, 5, 'O', 7, 8, 9].freeze
       expect(board.modify_board(5, true)).to eql(true)
+    end
+  end
+
+  describe '#win_or_draw' do
+    it 'returns false if the accu is equal to 9' do
+      expect(board.win_or_draw(9)).to eql(false)
+    end
+
+    it 'returns false if the player does a winning move' do
+      board = Board.new(B, testa)
+      expect(board.win_or_draw(5)).to eql(false)
+    end
+
+    it "returns true if there isn't a winning move or accu < 9" do
+      board = Board.new(B, testb)
+      expect(board.win_or_draw(3)).to eql(true)
     end
   end
 end
